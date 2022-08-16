1683deb751a448081a2ac2600fb2d039af6b3b22
SALTUK YAŞAR <46872799+saltukyasar@users.noreply.github.com>
2022-08-16 17:48:38 +0200

    courses creating and listing

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,13 +1,28 @@
 const Course = require('../models/Course');
 
 exports.createCourse = async (req, res) => {
-  const courses = await Course.create(req.body);
+  const course = await Course.create(req.body);
   try {
     res.status(201).json({
       status: 'success',
       course,
     });
-  } catch {
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.getAllCourses = async (req, res) => {
+  const courses = await Course.find();
+  try {
+    res.status(200).render('courses',{
+      courses,
+      page_name:'courses',
+    });
+  } catch (error) {
     res.status(400).json({
       status: 'fail',
       error,
