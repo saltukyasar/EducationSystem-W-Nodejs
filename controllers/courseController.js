3945d02fb6a6de616e8f8d1b8f5a8e75f8fc1d4e
SALTUK YAŞAR <46872799+saltukyasar@users.noreply.github.com>
2022-08-24 22:25:04 +0200

    course-teacher relation

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -2,7 +2,12 @@ const Course = require('../models/Course');
 const Category = require('../models/Category');
 
 exports.createCourse = async (req, res) => {
-  const course = await Course.create(req.body);
+  const course = await Course.create({
+    name: req.body.name,
+    description: req.body.description,
+    category: req.body.category,
+    user: req.session.userID
+  });
   try {
     res.status(201).redirect('/courses');
   } catch (error) {
@@ -43,7 +48,7 @@ exports.getAllCourses = async (req, res) => {
 };
 
 exports.getCourse = async (req, res) => {
-  const course = await Course.findOne({slug: req.params.slug});
+  const course = await Course.findOne({slug: req.params.slug}).populate('user');
   try {
     res.status(200).render('course',{
       course,
