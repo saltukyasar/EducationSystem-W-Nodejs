20079cb2f8d36d81f52539ddd56e2eabfd8a382b
SALTUK YAŞAR <46872799+saltukyasar@users.noreply.github.com>
2022-08-27 16:30:02 +0200

    validation processes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,10 @@
 const User = require('../models/User');
-const bcrypt = require ('bcrypt');
-const session = require('express-session')
+const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
+const session = require('express-session');
 
 const Category = require('../models/Category');
 const Course = require('../models/Course');
-
 
 exports.createUser = async (req, res) => {
   try {
@@ -12,27 +12,36 @@ exports.createUser = async (req, res) => {
 
     res.status(201).redirect('/login');
   } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      error,
-    });
+    const errors = validationResult(req);
+    console.log(errors);
+    console.log(errors.array()[0].msg);
+
+    for (let i = 0; i < errors.array().length; i++) {
+      req.flash('error', `${errors.array()[i].msg} `);
+    }
+    res.status(400).redirect('/register');
   }
 };
-exports.loginUser =  (req, res) => {
+exports.loginUser = (req, res) => {
   try {
-    const {email, password} = req.body;
-     User.findOne({email}, (err, user)=>{
-      if(user){
-        bcrypt.compare(password, user.password, (err, same)=>{
-          
+    const { email, password } = req.body;
+    User.findOne({ email }, (err, user) => {
+      if (user) {
+        bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
             // user session
             req.session.userID = user._id;
             res.status(200).redirect('/users/dashboard');
-          
-        })
+          } else {
+            req.flash('error', "Your password is nor correct!");
+            res.status(400).redirect('/login');
+          }
+        });
+      }else{
+        req.flash('error', "User is not exist!");
+            res.status(400).redirect('/login');
       }
-    })
-    
+    });
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -42,19 +51,21 @@ exports.loginUser =  (req, res) => {
 };
 
 exports.logoutUser = (req, res) => {
-  req.session.destroy(()=>{
+  req.session.destroy(() => {
     res.redirect('/');
-  })
-}
+  });
+};
 
 exports.getDashboardPage = async (req, res) => {
-  const user = await User.findOne({_id: req.session.userID}).populate('courses');
+  const user = await User.findOne({ _id: req.session.userID }).populate(
+    'courses'
+  );
   const categories = await Category.find();
-  const courses = await Course.find({user:req.session.userID})
+  const courses = await Course.find({ user: req.session.userID });
   res.status(200).render('dashboard', {
     page_name: 'dashboard',
     user,
     categories,
-    courses
+    courses,
   });
 };
