dea95464f2686ec6aa8a286b1db33fae4dbc0d57
SALTUK YAŞAR <46872799+saltukyasar@users.noreply.github.com>
2022-08-27 17:06:58 +0200

    courses can be deleted

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
+const methodOverride = require('method-override');
 const app = express();
 const pageRoute = require('./routes/pageRoute')
 const courseRoute = require('./routes/courseRoute')
@@ -40,7 +41,10 @@ app.use(flash());
 app.use((req,res,next)=>{
   res.locals.flashMessages = req.flash();
   next(); 
-})
+});
+app.use(methodOverride('_method',{
+  methods: ['POST', 'GET'],
+}))
 
 
 //Routes
