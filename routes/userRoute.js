a524653afbfa3ab3bbc27402704f572d87821a75
SALTUK YAŞAR <46872799+saltukyasar@users.noreply.github.com>
2022-08-28 17:07:50 +0200

    finished project

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -27,6 +27,7 @@ authController.createUser);
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
 router.route('/dashboard').get(authMiddleware, authController.getDashboardPage);
+router.route('/:id').delete(authController.deleteUser);
 
 
 module.exports = router;
